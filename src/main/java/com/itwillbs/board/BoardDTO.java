26d2524d2f507316e4912d2ff801a1df5691285d
touslessoirs <lemimml1@gmail.com>
2022-09-14 23:29:20 +0900

    boardDTO

---
+++ src/main/java/com/itwillbs/board/BoardDTO.java
@@ -0,0 +1,24 @@
+package com.itwillbs.board;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+public class BoardDTO {
+
+	// 변수 선언
+	private int bno;
+	private String name;
+	private String pass;
+	private String subject;
+	private String content;
+	private int readcount;
+	private int re_ref;
+	private int re_lev;
+	private int re_seq;
+	private Date date;
+	private String ip;
+	private String file;
+	
+	
+	
+}
