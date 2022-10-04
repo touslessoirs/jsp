77f09c476bfd00db9926c8e30baab88f6533259f
touslessoirs <lemimml1@gmail.com>
2022-10-04 22:37:37 +0900

    getMember()

--- src/main/java/com/itwillbs/member/MemberDAO.java
+++ src/main/java/com/itwillbs/member/MemberDAO.java
@@ -39,4 +39,93 @@ public class MemberDAO {
 	}
 	// DB 연결 메서드 - getConnection()
 	
+	/**
+	 * JavaDoc 주석 : 문법에 대한 설명을 작성하는 주석문
+	 * 
+	 * 이 메서드는 로그인 정보 ID를 입력받아서
+	 * id에 해당하는 정보 모두를 조회 후 MemberBean이라는 객체에 담아서
+	 * 리턴하는 메서드입니다. 
+	 */
+	
+	/*
+	public MemberBean getMember(String id) throws Exception {
+		// DB에서 정보 조회(select) -> MemberBean 객체 만들기
+		// DB에서 필요한 정보 가져오기
+		// DB연결정보(상수)
+		final String DRIVER = "com.mysql.cj.jdbc.Driver";
+		final String DBURL = "jdbc:mysql://localhost:3306/jspdb";
+		final String DBID = "root";
+		final String DBPW = "1234";
+		// 1. 드라이버 로드
+		Class.forName(DRIVER);
+	    // 2. 디비 연결
+	    Connection con = DriverManager.getConnection(DBURL, DBID, DBPW);
+		// 3. SQL 작성 & pstmt
+	    String sql = "select * from itwill_member where id=?";
+		PreparedStatement pstmt = con.prepareStatement(sql);
+		//???
+		pstmt.setString(1, id);
+		// 4. SQL문 실행
+		ResultSet rs = pstmt.executeQuery();
+		// 5. 데이터 처리
+		// MemberBean mb = new MemberBean();
+		MemberBean mb = null;
+	 	if(rs.next()){
+			// 회원정보 저장 (MemberBean)
+			// rs(DB정보) -> MemberBean
+	 		mb = new MemberBean();
+	 		
+	 		mb.setAge(rs.getInt("age"));
+	 		mb.setEmail(rs.getString("email"));
+	 		mb.setGender(rs.getString("gender"));
+	 		mb.setId(rs.getString("id"));
+	 		mb.setName(rs.getString("Name"));
+	 		mb.setPw(rs.getString("pw"));
+	 		mb.setRegdate(rs.getTimestamp("regdate"));
+	 	}
+	 	
+	 	// MemberBean 객체 생성 완료
+	 	System.out.println(" 정보 조회 완료! mb 리턴");
+		return mb;
+	}*/
+	
+	// 정보 조회 메서드 - getMember()
+	public MemberBean getMember(String id) throws Exception {
+
+		MemberBean mb = null;
+		
+		// 1. 드라이버 로드 + 2. DB 연결
+		try {
+			con = getConnection();
+			// 3. SQL 작성 & pstmt
+			sql = "select * from itwill_member where id=?";
+			PreparedStatement pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, id);
+			// 4. SQL문 실행
+			rs = pstmt.executeQuery();
+			// 5. 데이터 처리
+			if(rs.next()) {
+				// 입력한 id에 해당하는 정보가 있으면 : mb 객체 생성 후 저장
+		        mb = new MemberBean();
+		        mb.setAge(rs.getInt("age"));
+		        mb.setEmail(rs.getString("email"));
+		        mb.setGender(rs.getString("gender"));
+		        mb.setId(rs.getString("id"));
+		        mb.setName(rs.getString("Name"));
+		        mb.setPw(rs.getString("pw"));
+		        mb.setRegdate(rs.getTimestamp("regdate"));
+		    }
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		// 생성된 mb 객체 리턴
+		System.out.println(" 정보 조회 완료! mb 리턴");
+		return mb;
+	}
+	
+	// 정보 조회 메서드 - getMember()
+	
+	
 }	// MemberDAO 클래스 끝
